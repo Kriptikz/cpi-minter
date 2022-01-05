514622a46a3d69b8d09164c633c75c5986651737
Kriptikz <cruzxkriptikz@gmail.com>
2022-01-05 18:05:34 +0100

    Added _ to ctx in the initialize instruction method because we don't actually need to use it, Anchor does the deserialization and verification for us already. Created the minter program function that will mint 100 of the specified MintToken to the specified Account Created the minter Context struct Created an implementation to convert the Minter Context into a MintTo CpiContext

--- programs/cpi-minter/src/lib.rs
+++ programs/cpi-minter/src/lib.rs
@@ -1,14 +1,49 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod cpi_minter {
+
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+
+    pub fn minter(ctx: Context<Minter>) -> ProgramResult {
+        // Mint to an account
+        token::mint_to(
+            ctx.accounts.into_mint_to_context(),
+            100,
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Minter<'info> {
+    #[account(mut)]
+    pub initializer_token_account: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    pub rent: Sysvar<'info, Rent>,
+    pub token_program: Program<'info, Token>,
+}
+
+impl<'info> Minter<'info> {
+    fn into_mint_to_context(&self) -> CpiContext<'_, '_, '_, 'info, MintTo<'info>> {
+        let cpi_accounts = MintTo {
+            mint: self.mint.to_account_info(),
+            to: self.initializer_token_account.to_account_info(),
+            authority: self.authority.to_account_info(),
+        };
+
+        CpiContext::new(self.token_program.to_account_info(), cpi_accounts)
+    }
+}
